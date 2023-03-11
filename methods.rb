24903a4112477dae0d2ef73964c3fca4ad5347fb
Joyce <joycenjoroge47@gmail.com>
2023-03-11 22:39:52 +0300

    Completed lab

--- methods.rb
+++ methods.rb
@@ -1,1 +1,29 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+
+end
+
+greet_programmer
+
+def greet(name)
+    puts "Hello, #{name}!"
+  end
+
+  greet "Joyce"
+
+  def greet_with_default(name="programmer")
+    puts "Hello, #{name}!"
+  end
+
+  greet_with_default
+
+  def add (num1, num2 )
+     num1 + num2
+  end
+ 
+def halve(num)
+    if (num.class==Integer)
+        num/2
+    end
+end
